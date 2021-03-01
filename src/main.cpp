6eda802dc6d9ec993a46850c36af1aa2e83048ef
Samuel Li <samuelgh.li@gmail.com>
2021-03-02 02:34:19 +1100

    new code push

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,45 @@
 #include "Servo.h"
 
-
-int servo_pin = D7;  // for ESP8266 microcontroller
-
+int servo_pin = D7; // for ESP8266 microcontroller
+int count = 0;
 
 Servo myservo;
-int count=0;
- 
-void setup() 
-{ 
+
+void setup()
+{
+
   myservo.attach(servo_pin);
- Serial.begin(9600);
- myservo.write(5);
- delay(2000);
-} 
-  
-void loop() 
-{ 
- if(count!=4){
-  myservo.write(180);
-  delay(1000);
+  Serial.begin(9600);
   myservo.write(5);
-  delay(1000);
-  count++;
- }
- 
- 
- 
-
+  delay(2000);
 }
 
-void push(){ 
-
+void loop()
+{
+  if (count != 4)
+  {
+    myservo.write(180);
+    delay(1000);
+    myservo.write(5);
+    delay(1000);
+    count++;
+  }
+  else
+  {
+    if (count == 4)
+    {
+      myservo.write(5);
+   
+     // yield();
+    }
+  }
 }
 
-void back(){
-
+void push()
+{
+  myservo.write(180);
+    delay(1000);
+    myservo.write(5);
+    delay(1000);
 }
+
