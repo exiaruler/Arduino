34b128869f47582b0ac703ce356a564e24781c83
Samuel Li <samuelgh.li@gmail.com>
2021-03-01 21:57:09 +1100

    first proto exia function

--- src/main.cpp
+++ src/main.cpp
@@ -5,57 +5,35 @@ int servo_pin = D7;  // for ESP8266 microcontroller
 
 
 Servo myservo;
-int angle = 0;  
-int count;
+int count=0;
  
 void setup() 
 { 
   myservo.attach(servo_pin);
  Serial.begin(9600);
+ myservo.write(5);
+ delay(2000);
 } 
   
 void loop() 
-{ // move from 0 to 180 degrees with a positive angle of 1
-
-
-  while(count!=4){
-    for(angle = 0; angle < 180; angle += 1)
-  {                          
-    myservo.write(angle);
-    delay(15); 
-                     
-  } 
-  //Serial.print("forward");  
+{ 
+ if(count!=4){
+  myservo.write(180);
   delay(1000);
-  
-  // move from 180 to 0 degrees with a negative angle of 5
-  
-  for(angle = 180; angle>=1; angle-=5)
-  {                                
-    myservo.write(angle);
-    delay(5);     
-                        
-  } 
-  //Serial.print("backward");       
-    //delay(1000);
-  }
+  myservo.write(5);
+  delay(1000);
   count++;
-  Serial.print(count);
+ }
+ 
+ 
+ 
+
 }
 
 void push(){ 
-    for(angle = 0; angle < 180; angle += 1)
-  {                          
-    myservo.write(angle);
-    delay(15);                       
-  } 
+
 }
 
 void back(){
-  for(angle = 180; angle>=1; angle-=5)
-  {                                
-    myservo.write(angle);
-    delay(5);                       
-  } 
 
 }
