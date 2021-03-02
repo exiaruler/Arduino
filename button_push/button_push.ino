8dc183425be07530634d79df48124f8236941170
Samuel Li <samuelgh.li@gmail.com>
2021-03-03 00:51:16 +1100

    Bug free push exia

--- button_push/button_push.ino
+++ button_push/button_push.ino
@@ -1,13 +1,13 @@
 #include "Servo.h"
 
 int servo_pin = D7; // for ESP8266 microcontroller
-int count = 0;
-
+int ExiaMode = 4;
+String activate="on";
 Servo myservo;
 
 void setup()
 {
-
+ // pinMode(6, OUTPUT);
   myservo.attach(servo_pin);
   Serial.begin(9600);
   myservo.write(5);
@@ -16,29 +16,54 @@ void setup()
 
 void loop()
 {
-  if (count != 4)
-  {
-    myservo.write(180);
-    delay(1000);
-    myservo.write(5);
-    delay(1000);
-    count++;
-  }
-  else
-  {
-    if (count == 4)
-    {
-      myservo.write(5);
-   
-     // yield();
+  if(activate=="on"){
+    for(int i=0; i<5; i++){
+    push();
     }
+    activate="off";
+    
   }
+  
+  while(activate=="off"){
+        
+        myservo.write(5);
+        delay(1000);
+     
+  }
+  
+    
 }
 
 void push()
 {
   myservo.write(180);
-    delay(1000);
+    delay(3000);
     myservo.write(5);
     delay(1000);
+}
+
+void exiaStart(){
+  push();
+}
+
+void exiaNormal(){
+  for(int i=0; i<2; i++){
+    push();
+  }
+}
+
+void exiaPretrans(){
+  for(int i=0; i<3; i++){
+    push();
+  }
+}
+void exiaTransAm(){
+  for(int i=0; i<4; i++){
+    push();
+  }
+}
+void exiaCycle(){
+  for(int i=0; i<5; i++){
+    push();
+  }
 }
